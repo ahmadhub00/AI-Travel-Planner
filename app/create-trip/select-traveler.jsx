408ed9757a652bc634eb8bba5c5542d975fb549c
Ahmad Taimur <ullahtaimur@gmail.com>
2025-03-11 21:42:06 +0500

    commit

--- app/create-trip/select-traveler.jsx
+++ app/create-trip/select-traveler.jsx
@@ -1,17 +1,17 @@
-import { View, Text, FlatList } from 'react-native'
+import { View, Text, FlatList,TouchableOpacity } from 'react-native'
 import React, { useEffect, useContext }  from 'react'
 import { useNavigation ,useRouter} from 'expo-router';
 import { SelectTravelesList } from '../../constants/Options';
 import OptionCard from '../../components/CreateTrip/OptionCard';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function SelectTraveler() {
   const navigation=useNavigation(); 
+  const router = useRouter(); 
 
     useEffect (()=>{ 
     navigation.setOptions({
-    headerShown:true, 
-    headerTransparent:true, 
-    headerTitle:''
+    headerShown:false 
     })
      },[])
   
@@ -21,6 +21,13 @@ export default function SelectTraveler() {
       paddingTop: 75, 
       backgroundColor:"white",
       height:'100%'  }}>
+
+        <TouchableOpacity
+                    onPress={() => router.push('/create-trip/search-place')}
+                    style={{ padding: 10 }}>
+                
+                    <Ionicons name="arrow-back" size={28} color="black" />
+                </TouchableOpacity>
 
     <Text  style={{ 
      fontSize: 35, 
