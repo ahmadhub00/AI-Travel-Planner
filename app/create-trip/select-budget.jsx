c061cd8f807b4bf326337b89f8ec71f4e08173e8
Ahmad Taimur <ullahtaimur@gmail.com>
2025-03-13 04:11:49 +0500

    Budget complete

--- app/create-trip/select-budget.jsx
+++ app/create-trip/select-budget.jsx
@@ -1,20 +1,36 @@
-import { View, Text, FlatList, TouchableOpacity } from 'react-native'
-import React ,{ useEffect,useState} from 'react'
+import { View, Text, FlatList, TouchableOpacity, Platform, Alert, ToastAndroid } from 'react-native'
+import React ,{ useEffect,useState, useContext} from 'react'
 import Ionicons from '@expo/vector-icons/Ionicons';
 import OptionCard from '../../components/CreateTrip/OptionCard';
 import { useRouter, useNavigation } from 'expo-router';
 import { SelectBudgetOptions } from '../../constants/Options';
+import {CreateTripContext} from '../../context/CreateTripContext';
 
 
 export default function SelectBudget() {
     const router = useRouter();
     const navigation=useNavigation();
-const [selectedOption, setSelectedOption] = useState(null);
-    useEffect (()=>{
+    const [selectedOption, setSelectedOption] = useState(null);
+    const {tripData,setTripData}=useContext(CreateTripContext);
+
+useEffect (()=>{
         navigation.setOptions({
           headerShown:false 
         })
        },[])
+
+       
+         const onClickContinue = () => {
+               if (!selectedOption) {
+                   const message = 'Select your Budget';
+                   if (Platform.OS === 'android') {
+                       ToastAndroid.show(message, ToastAndroid.LONG);
+                     } else { Alert.alert( message); }
+                   return; }
+                   setTripData({ ...tripData, budget: selectedOption });
+                   
+       
+               };
 
   return (
     <View style={{
@@ -50,10 +66,24 @@ const [selectedOption, setSelectedOption] = useState(null);
             onPress={() => setSelectedOption(item)}
             style={{ marginVertical: 10 }}> 
            <OptionCard option={item} selectedOption={selectedOption}/>
-           </TouchableOpacity> )}
-           
-             />
+           </TouchableOpacity> )}/>
     </View>
+
+    <TouchableOpacity 
+         onPress={ onClickContinue}
+         style={{
+          padding:15,
+          backgroundColor:"black",
+          borderRadius:15,
+          marginTop:20
+         }}>
+          <Text style={{
+            color:"white",
+            textAlign:"center",
+            fontSize:20,
+            fontFamily:'outfit-medium'
+          }}>Continue</Text>
+         </TouchableOpacity>
     </View>
   )
 }
