a04e8a765f333984bdc3df0c94dca135b7f336fa
Ahmad Taimur <ullahtaimur@gmail.com>
2025-03-12 23:58:03 +0500

    commit

--- app/create-trip/select-dates.jsx
+++ app/create-trip/select-dates.jsx
@@ -1,13 +1,19 @@
-import { View, Text,TouchableOpacity } from 'react-native'
+import { View, Text,TouchableOpacity, ToastAndroid } from 'react-native'
 import React , { useEffect, useContext ,useState}from 'react'
 import { Link, useNavigation ,useRouter} from 'expo-router';
 import Ionicons from '@expo/vector-icons/Ionicons';
+import CalendarPicker from 'react-native-calendar-picker';
+import moment from 'moment';
+import { set } from 'date-fns';
+import { CreateTripContext } from '../../context/CreateTripContext';
+
 export default function SelectDates() {
   
     const router = useRouter(); 
   const navigation=useNavigation(); 
 const [ startDate, setStartDate ] = useState();
 const [ endDate, setEndDate ] = useState();
+const [ tripData, setTripData ] = useContext(CreateTripContext);
 
   useEffect (()=>{ 
         navigation.setOptions({
@@ -16,20 +22,38 @@ const [ endDate, setEndDate ] = useState();
        },[])
        
        const onDateChange = (date, type) => {
-        console.log(type)
-        console.log(date)}
+        console.log(date,type);
+        
+        if(type==='START_DATE')
+          {setStartDate(moment(date))}
+        else{
+          setEndDate(moment(date))}
+        }
+        
        const OnDateSectionContinue = () => {
-      
-    }
+      if(!startDate&&!endDate){
+        ToastAndroid.show("Please select the dates", ToastAndroid.LONG);
+      return;
+      }
+        const totalNoOfDays = endDate.diff(startDate, 'days');
+        console.log(totalNoOfDays+1); 
+        setTripData({...tripData,
+          startDate:startDate,
+          endDate:endDate,
+          totalNoOfDays:totalNoOfDays+1});
+          router.push ('/create-trip/select-budget');
+        }
+    
+  
 
-    return (
+    return ( 
     <View style={{
         padding: 25,
         paddingTop: 75,
         backgroundColor: "white",
         height: '100%'
     }}>
-        <TouchableOpacity
+    <TouchableOpacity
       onPress={() => router.push('/create-trip/select-traveler')}
       style={{ padding: 10 }}>
       <Ionicons name="arrow-back" size={28} color="black" />
@@ -40,6 +64,7 @@ const [ endDate, setEndDate ] = useState();
         fontFamily: 'outfit-bold', 
         marginTop: 20,
       }}>Travel Dates</Text>
+       
       <View style={{
         marginTop: 30
       }}>
@@ -47,16 +72,18 @@ const [ endDate, setEndDate ] = useState();
           fontSize: 23, 
           fontFamily: 'outfit-bold'
         }}>Choose your travel dates</Text>*/}
-      {/*<CalendarPicker onDateChange={onDateChange}
+
+      <CalendarPicker onDateChange={onDateChange}
       allowRangeSelection={true} 
       minDate={new Date()}
       maxRangeDuration={5}
       selectedRangeStyle={{
         backgroundColor:"black"}}
         selectedDayTextStyle={{
-            color:"white"}}/>     */}
+            color:"white"}}/>     
     </View>
     <TouchableOpacity
+    onPress={OnDateSectionContinue}
       style={{ padding: 15,
       backgroundColor:"black",
       borderRadius:15,
