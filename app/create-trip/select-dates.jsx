43aa7d1457d078338af07edd61de471d61230600
Ahmad Taimur <ullahtaimur@gmail.com>
2025-03-12 05:34:55 +0500

    calender activated

--- app/create-trip/select-dates.jsx
+++ app/create-trip/select-dates.jsx
@@ -1,11 +1,37 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-
+import { View, Text,TouchableOpacity } from 'react-native'
+import React , { useEffect, useContext ,useState}from 'react'
+import { Link, useNavigation ,useRouter} from 'expo-router';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import CalendarPicker from 'react-native-calendar-picker';
 export default function SelectDates() {
-  
+  const router = useRouter(); 
+  const navigation=useNavigation(); 
+
+  useEffect (()=>{ 
+        navigation.setOptions({
+          headerShown:false 
+        })
+       },[])
     return (
-    <View>
-      <Text>Select Dates</Text>
+    <View style={{
+        padding: 25,
+        paddingTop: 75,
+        backgroundColor: "white",
+        height: '100%'
+    }}>
+        <TouchableOpacity
+      onPress={() => router.push('/create-trip/select-traveler')}
+      style={{ padding: 10 }}>
+      <Ionicons name="arrow-back" size={28} color="black" />
+    </TouchableOpacity>
+
+      <Text style={{
+        fontSize: 35, 
+        fontFamily: 'outfit-bold', 
+        marginTop: 20,
+        
+      }}>Travel Dates</Text>
+      <CalendarPicker onDateChange={this.onDateChange} />     
     </View>
   )
 }
