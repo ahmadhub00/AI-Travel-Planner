4ed1ebe6247cdf3495fe97dc8661299877893c11
Ahmad Taimur <ullahtaimur@gmail.com>
2025-03-12 23:59:27 +0500

    commit

--- app/create-trip/select-budget.jsx
+++ app/create-trip/select-budget.jsx
@@ -1,7 +1,16 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { use } from 'react';
 
 export default function SelectBudget() {
+    const navigation=useNavigation();
+
+    useEffect (()=>{
+        navigation.setOptions({
+          headerShown:false 
+        })
+       },[])
+       
   return (
     <View>
       <Text>SelectBudget
