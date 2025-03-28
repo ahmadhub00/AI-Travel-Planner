997e4d2b03a7b8615c5e9083ce8a0edf5fd40f96
Ahmad Taimur <ullahtaimur@gmail.com>
2025-03-28 12:00:00 +0000

    commit

--- app/trip-details/index.jsx
+++ app/trip-details/index.jsx
@@ -76,11 +76,10 @@ export default function TripDetails() {
 console.log("Trip Details:", ParsedTripData.tripPlan?.tripDetails);
 console.log("Location:", ParsedTripData.tripPlan?.tripDetails?.location);console.log("Full ParsedTripData:", JSON.stringify(ParsedTripData, null, 2));
   return (
-    <View style={{ marginTop: 20, padding: 15 }}>
-      <TouchableOpacity onPress={() => router.back()} style={{ padding: 10 }}>
-        <Ionicons name="arrow-back" size={28} color="black" />
-      </TouchableOpacity>
-
+   <View>
+     <View style={{ position: 'relative' }}>
+    
+    <View >
       {ParsedTripData?.locationInfo?.photoRef ? (
         <Image
           source={{
@@ -90,19 +89,34 @@ console.log("Location:", ParsedTripData.tripPlan?.tripDetails?.location);console
           }}
           style={{
             width: '100%',
-            height: 300,
-            borderRadius: 15
+            height: 300
           }}
         />
       ) : (
         <Text>No Image Available</Text>
-      )}
+      )}</View>
+      <View style={{
+        borderRadius: 50,
+         position: 'absolute',
+         top:35,
+         left:2
+         }}>
+              <TouchableOpacity onPress={() => router.back()} style={{ padding: 10 }} hitSlop={{ top: 20, bottom: 20, left: 20, right: 20 }}>
+        <Ionicons name="arrow-back" size={30} color="white" />
+      </TouchableOpacity></View>
+</View>
 
-      <View>
-        <Text>{TripData?.tripPlan?.tripDetails?.location || "Location Not Available"}</Text>
-      
-      
+      <View style={{
+        pading:15,
+        backgroundColor:'white',
+        height:'100%',
+        marginTop:-30,
+        borderTopLeftRadius:30,
+        borderTopRightRadius:30
+      }}>
+        <Text style={{padding:10}}>{LatestTrip?.tripPlan?.tripDetails?.location || "Location Not Available"}</Text>
       </View>
+      
     </View>
   );
 }
